c63998e9fe1f7c70bf6d7a75c8d9ccaa85252766
S M Ammad Ali <65030609+SMAmmadAli@users.noreply.github.com>
2022-12-29 07:31:34 -0800

    API with ListTile

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_api/Models/posts_model.dart';
+import 'package:flutter_api/Models/photo_model.dart';
 import 'package:http/http.dart' as http;
 
 class MyHomePage extends StatefulWidget {
@@ -13,15 +13,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<PostsModel> postList = [];
+  List<PhotesModel> postList = [];
 
-  Future<List<PostsModel>> getPostApi() async {
-    final response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+  Future<List<PhotesModel>> getPostApi() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
     var data = jsonDecode(response.body.toString());
     if (response.statusCode == 200) {
       for (Map<String, dynamic> i in data) {
-        postList.add(PostsModel.fromJson(i));
+        postList.add(PhotesModel.fromJson(i));
       }
       return postList;
     } else {
@@ -32,6 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffE5E5E5),
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -47,15 +48,99 @@ class _MyHomePageState extends State<MyHomePage> {
                     return ListView.builder(
                         itemCount: postList.length,
                         itemBuilder: ((context, index) {
-                          return Card(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(postList[index].title.toString()),
-                                Text(postList[index].title.toString())
-                              ],
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 10.0),
+                            child: Card(
+                              elevation: 2,
+                              child: ListTile(
+                                  leading: ConstrainedBox(
+                                    constraints: const BoxConstraints(
+                                      minWidth: 100,
+                                      minHeight: 260,
+                                      maxWidth: 104,
+                                      maxHeight: 264,
+                                    ),
+                                    child: Image.network(
+                                        postList[index].url.toString(),
+                                        fit: BoxFit.cover),
+                                  ),
+                                  title: Text("Item No." +
+                                      postList[index].id.toString()),
+                                  subtitle: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(postList[index].title.toString()),
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          InkWell(
+                                            onTap: () {},
+                                            child: Container(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      4, 1.5, 4, 1.5),
+                                              decoration: BoxDecoration(
+                                                border: Border.all(
+                                                  color: Colors.green,
+                                                  width: 2.0,
+                                                ),
+                                              ),
+                                              child: const Text(
+                                                "Email",
+                                                style: TextStyle(
+                                                    color: Colors.green,
+                                                    fontWeight: FontWeight.w500,
+                                                    fontSize: 14),
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            width: 3,
+                                          ),
+                                          InkWell(
+                                            onTap: () {},
+                                            child: Container(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      4, 1.5, 4, 1.5),
+                                              decoration: BoxDecoration(
+                                                border: Border.all(
+                                                  color: Colors.green,
+                                                  width: 2.0,
+                                                ),
+                                              ),
+                                              child: const Text(
+                                                "CALL",
+                                                style: TextStyle(
+                                                    color: Colors.green,
+                                                    fontWeight: FontWeight.w500,
+                                                    fontSize: 14),
+                                              ),
+                                            ),
+                                          ),
+                                          IconButton(
+                                              onPressed: () {},
+                                              icon: const Icon(
+                                                Icons.whatsapp,
+                                                color: Colors.green,
+                                              )),
+                                        ],
+                                      ),
+                                    ],
+                                  )),
                             ),
                           );
+                          // return Card(
+                          //   child: Column(
+                          //     crossAxisAlignment: CrossAxisAlignment.start,
+                          //     children: [
+                          //       Text(postList[index].title.toString()),
+                          //       Text(postList[index].title.toString())
+                          //     ],
+                          //   ),
+                          // );
                         }));
                   }
                 }),
